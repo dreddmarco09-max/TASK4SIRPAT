8b8767c15ac138360b49dd9034a8b2736f5d85cc
ChesterMercado <chesterlewizmercado@gmail.com>
2026-03-14 23:17:25 +0800

    implement change

--- Hardware.java
+++ Hardware.java
@@ -8,6 +8,7 @@ abstract class Hardware {
         this.id = id;
         this.brand = brand;
         this.spec = spec;
+    
     }
 
     
@@ -17,33 +18,41 @@ abstract class Hardware {
     
     public int getSpec() {
         return spec;
+   
     }
 }
 
 class Laptop extends Hardware {
     public Laptop(int id, String brand, int spec) {
         super(id, brand, spec);
+    
     }
 
     
     @Override
     public String getInterpretation() {
        
+        
         return id + "\t" + brand + "\t\t" + spec + "\tLaptop\t" + spec + "GB RAM";
+    
     }
 }
+
 
 
 
 class Phone extends Hardware {
     public Phone(int id, String brand, int spec) {
         super(id, brand, spec);
+    
     }
 
    
+    
     @Override
     public String getInterpretation() {
         
         return id + "\t" + brand + "\t\t" + spec + "\tPhone\t" + spec + " Megapixels";
+    
     }
 }
